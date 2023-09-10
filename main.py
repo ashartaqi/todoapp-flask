09c9f50c0d5ad25610f00d7289518facd3e7f248
ashartaqi <ashartaqi12@gmail.com>
2023-09-10 17:38:55 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from website import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+    app.debug = True
+    app.run()
---
+++ website/views.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import login_required, current_user
+from . import db
+from .models import Note
+
+views = Blueprint('views',__name__)
+
+@login_required
+@views.route('/', methods=["GET", "POST"])
+def home():
+    if request.method == 'GET':
+        return render_template("home.html", user=current_user)
+
+    if request.method == 'POST':
+        note = request.form.get('note')
+        print(note)
+        if len(note) < 1:
+            flash("Note is too short", category="error") 
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash("Note added", category='success')
+            
+
+    return render_template("home.html", user=current_user)
